f4771afafcce5483d7a09ee7edcd083a72f8fdb7
Danilo Rodrigues De Lima <38114256+Danilolima180@users.noreply.github.com>
2023-05-20 14:04:55 -0300

    Aula1
    primeiros passos JS

---
+++ javascript/aula1/javascript.js
@@ -0,0 +1,16 @@
+//alert('Hello World');
+//Tipos de váriaveis
+mensagem = 'Meu primeiro Javascript';//string
+numero_inteiro = 23; // inteiro
+numero_float = 1.5; // float
+boolean= true ; // verdadeiro ou falso
+array = ['danilo', 35, 20,05,true];
+nome='Danilo';
+idade='35';
+altura= 1.69;
+maior_idade= true;
+cidade= 'Sumaré';
+numero_casa= 355;
+
+meus_dados = [nome,idade,altura,maior_idade,cidade,numero_casa];
+console.log('Uma mensagem no console.');
